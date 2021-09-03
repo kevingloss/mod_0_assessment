7be112b1f77945019d348fe2cc7c883451e9dbe1
Kevin Gloss <kgloss89@gmail.com>
2021-09-02 20:18:40 -0600

    Complete assessment model b

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+developer = {
+  "name" => "Kevin",
+  "program" => "Backend",
+  "turing_cohort" => 2110,
+  "age" => 31,
+  "knows_ruby" => false,
+  "turing_excited" => true,
+  "pronouns" => ["he", "him", "his"],
+  "experience" => ["none", 0, "nil"]
+}
