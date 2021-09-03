5580e6cb7dc8dd3506aa810f051b4dafe38514f7
Kevin Gloss <kgloss89@gmail.com>
2021-09-02 20:14:39 -0600

    Complete assessment model a

---
+++ models/model_a.rb
@@ -0,0 +1,11 @@
+dinosaur = {
+  "name" => "Velociraptor"
+  "time_period" => "Late Cretaceous Period",
+  "in_movie" => "Jurassic Park",
+  "lived_years_ago" => 71000000,
+  "height_feet" => 3,
+  "can_fly" => false,
+  "fossil_found" => true,
+  "other_carnivores" => ["Tyrannosaurus Rex", "Allosaurus", "Spinosaurus"],
+  "prey" => ["small reptiles", "amphibians", "mammals"]
+}
