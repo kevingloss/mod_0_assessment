ca77438742b36e34c9802325c572421883f5516b
Kevin Gloss <kgloss89@gmail.com>
2021-09-02 20:07:28 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,14 @@
+movie = "Lord of the Rings"
+is_entertaining = true
+movie = "Guardians of the Galaxy"
+
+num_4 = 4
+num_6 = 6
+
+product = num_4 * num_6
+
+favorite_foods = ["Cheeseburger", "Pizza", "Chimichanga"]
+
+favorite_foods.shift
+
+puts movie.length
